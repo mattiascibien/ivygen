b7f10fc32bbeaf5157d6f10d5b8a1769801e22de
Mattias Cibien <mattias@mattiascibien.net>
2015-05-08 12:53:33 +0200

    Adding CI scripts from https://github.com/Mudlet/Mudlet/

---
+++ CI/travis.linux.before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ev
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo apt-get update -qq
---
+++ CI/travis.linux.install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -ev
+sudo apt-get install -qq qt5-qmake qtbase5-dev
