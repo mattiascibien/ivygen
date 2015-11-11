71b19fb11bb143a95be8dcd9fedc994f7326d997
Mattias Cibien <mattias@mattiascibien.net>
2015-11-11 14:46:34 +0100

    Updated .travis.yml to use new travis architecture

--- CI/travis.linux.before_install.sh
+++ CI/travis.linux.before_install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 set -ev
-sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-sudo add-apt-repository --yes ppa:george-edison55/precise-backports
-sudo apt-get update -qq
--- CI/travis.linux.install.sh
+++ CI/travis.linux.install.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 set -ev
-sudo apt-get install -qq cmake
-sudo apt-get install -qq qtbase5-dev
