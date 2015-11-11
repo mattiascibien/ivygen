d0acdcf6afaed43431b0632a2db306897b413a4f
Mattias Cibien <mattias@mattiascibien.net>
2015-11-11 12:47:30 +0100

    Fixing cmake build

--- src/app/aboutdialog.cpp
+++ src/app/aboutdialog.cpp
@@ -6,7 +6,11 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+#ifdef GIT_VERSION 
     ui->labelVersion->setText(QString(GIT_VERSION));
+#else
+    ui->labelVersion->setText(QString("Unknown version"));
+#endif
 }
 
 AboutDialog::~AboutDialog()
